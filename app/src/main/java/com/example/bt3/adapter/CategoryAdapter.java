d498001c53f6ff29a9ddfe31c19636ff57bb129c
LuongThinh123 <luongchithinh123@gmail.com>
2022-12-12 20:40:10 +0700

    fix again

--- app/src/main/java/com/example/bt3/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/bt3/adapter/CategoryAdapter.java
@@ -20,17 +20,16 @@ public class CategoryAdapter extends ArrayAdapter<Category> {
     }
 
     public View getView (int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
         Category category = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.category, parent, false);
         }
-        // Lookup view for data population
+
         TextView tvCategory = (TextView) convertView.findViewById(R.id.categoryTitle);
-        // Populate the data into the template view using the data object
+
         tvCategory.setText(category.title);
-        // Return the completed view to render on screen
+
         return convertView;
     }
 }
--- app/src/main/java/com/example/bt3/adapter/ItemAdapter.java
+++ app/src/main/java/com/example/bt3/adapter/ItemAdapter.java
@@ -20,18 +20,17 @@ public class ItemAdapter extends ArrayAdapter<Item> {
     }
 
     public View getView (int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
+
         Item item = getItem(position);
-//        Item item = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
         }
-        // Lookup view for data population
+
         TextView tvItem = (TextView) convertView.findViewById(R.id.tvItem);
-        // Populate the data into the template view using the data object
+
         tvItem.setText(item.title);
-        // Return the completed view to render on screen
+
         return convertView;
     }
 }
